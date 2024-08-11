22dd376e2ecc36f42d09c8522ca21ec6d7781a53
Supphakon <Hopper.mew@gmail.com>
2024-08-11 15:41:57 +0700

    variable and opreators

---
+++ python.py
@@ -0,0 +1,88 @@
+"""
+#
+# Part: Python Comment
+#
+"""
+
+# this is comment
+# v = s/t
+# v = ความเร็ว (m/s)
+# s = ระยะทาง (m)
+# t = เวลา (s)
+
+"""
+v = s/t
+v = ความเร็ว (m/s)
+s = ระยะทาง (m)
+t =เวลา (s)
+"""
+
+print ("Hello world!!!");
+
+"""
+#
+# Part : Python variables
+#
+"""
+x = 5
+y = "Hey Brus"
+print(x,y)
+
+x = str(3)
+y = int(5)
+z = float(7)
+print(type(x),type(y),type(z))
+
+"""
+#
+# part: Variables Names
+#
+"""
+myvar = "john"
+myvar = "john"
+_my_var = "john"
+MyVar = "john"
+MYAVR = "john"
+MY_VAR = "john"
+my_var2 = "john"
+
+#  Camel Case
+myVariableName = "john"
+# Pascal Case
+myVariableName = "john"
+# snake Case 
+my_variable_name = "john"
+
+"""
+#
+# Part: Python Strin
+#
+"""
+
+x = "Hey Brus"
+print(x)
+
+y ="""1 Hey Brus
+2 Hey Brus
+3 Hey Brus
+"""
+print(y)
+
+x = "Hey Brus"
+print(x[2])
+print(len(x))
+print("Hey" in x)
+print("what'sup" not in x)
+print(x.upper())
+print(x.lower())
+
+print(x.replace("Brus", 'Sis'))
+print(x.split(" "))
+
+a = "Apple"
+b = "Company"
+print(a = " " + b)
+
+price = 20
+word = f"Price: {price:.2f}"
+print(word)
