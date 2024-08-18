a480213e4c52f4af9943c768c36c1bcd3d9902af
Supphakon <Hopper.mew@gmail.com>
2024-08-18 14:29:51 +0700

    python conditions

--- array2.java
+++
@@ -1,7 +0,0 @@
-public class array2 {
-    public static void main(String[] args) {
-        int score[]=new int[6];
-        int x[]={4,9,10,33,45,88,};
-        System.out.println(x.length);
-    }
-}
--- for2.java
+++
@@ -1,8 +0,0 @@
-public class for2 {
-    public static void main(String[] args) {
-        for(int i =1; i <= 100; i++){
-            System.out.println(i);
-            i += 1;
-        }
-    }
-}
